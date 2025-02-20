db8a09a2c98f86ed71c8d3c00885b283e9629b5a
JuanLuque <juanluquegiron12@gmail.com>
2025-02-20 09:05:33 +0100

    Converted the LCD files into C files

--- Core/lcd.h
+++ Core/lcd.h
@@ -8,6 +8,9 @@
  #ifndef LCD_H_
  #define LCD_H_
 
+ #ifdef __cplusplus
+  extern "C" {
+  #endif
  /* Public variables ---------------------------------------------------------*/
  // This constant represents the range of x values our model was trained on,
  // which is from 0 to (2 * Pi). We approximate Pi to avoid requiring
@@ -15,4 +18,10 @@
  const float INPUT_RANGE = 2.f * 3.14159265359f;
  /* Public functions ---------------------------------------------------------*/
  void LCD_Init(void);
+ // Called by the main loop handle_output() to plot y vs x
+void LCD_Output(float x_value, float y_value);
+
+#ifdef __cplusplus
+}
+#endif
  #endif  // LCD_H_
