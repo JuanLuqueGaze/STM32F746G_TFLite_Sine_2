d0204bcc8d1a034ba3fa4bdab4db7496726dedcf
JuanLuque <juanluquegiron12@gmail.com>
2025-02-20 08:58:06 +0100

    Keep io_putchar() simple

--- Core/debug_log.cpp
+++ Core/debug_log.cpp
@@ -17,20 +17,13 @@ _BEGIN_STD_C
 #include "stm32f7xx_hal_uart.h"
 
 extern UART_HandleTypeDef DebugUartHandler;
-
-#ifdef __GNUC__
+ 
 int __io_putchar(int ch)
 {
 	HAL_UART_Transmit(&DebugUartHandler, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
     return ch;
 }
-#else
-int fputc(int ch, FILE *f)
-{
-    HAL_UART_Transmit(&DebugUartHandler, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
-    return ch;
-}
-#endif /* __GNUC__ */
+
 
 
 void DebugLog(const char *s)
